17acbeed6e8a0f716819d890d5f32f54a033a443
oscar-rivero <oscar.a.rivero95@gmail.com>
2022-03-01 16:49:05 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import nltk
 import streamlit as sl
 from modulo_cargar_tagger import tagger
+import pandas as pd
 
 nltk.download('punkt')
 
@@ -9,8 +10,8 @@ sl.write("Hello, this is a demonstration of text tagging with my own tagger.")
 
 txt = sl.text_area("Introduce the text you want to POS tag.")
 
-sl.write("You can use the following text, from the Featured Article of Wikipedia in English from 1 february 2022:")
-sl.write("Hector Waller (4 April 1900 - 1 March 1942) was a senior officer in the Royal Australian Navy. Born in Benalla, Victoria, Waller entered the Royal Australian Naval College in 1913 and served in the First World War. He gained his first command in 1937, as captain of the destroyer HMS Brazen. In September 1939, he took command of HMAS Stuart and four other obsolete destroyers that together became known as the \"Scrap Iron Flotilla\". Waller was awarded the Distinguished Service Order and Bar, and twice mentioned in despatches, for his achievements in the Mediterranean. He then transferred to the South West Pacific as captain of the light cruiser HMAS Perth, and went down with his ship against heavy odds during the Battle of Sunda Strait in early 1942. He received a third mention in despatches posthumously, and in 2011 came under formal consideration for the award of the Victoria Cross for his performance as Perth's captain. The submarine HMAS Waller is named in his honour.")
+sl.write("You can use the following text as an example (just copy-paste above):")
+sl.write("The dog barks at the car.")
 
 
 if txt:
@@ -20,8 +21,9 @@ if txt:
 
     tags_texto=tagger.tag(tokens_texto)
 
+    data = pd.DataFrame(tags_texto, columns=("Token", "POS Tag"))
     sl.write("The output:")
-    sl.write(tags_texto)
+    sl.write(data)
     
 
 
